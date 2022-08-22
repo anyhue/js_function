cc545dd7b8f0f35419764648768e7340ba9745b6
anyhue <111332914+anyhue@users.noreply.github.com>
2022-08-22 12:19:45 +0900

    js function

---
+++ app.js
@@ -0,0 +1,14 @@
+function sayHello(nameOfPerson, age) {
+    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+}
+
+sayHello("A", 10);
+sayHello("B", 20);
+sayHello("C", 30);
+
+function plus(a, b) {
+    console.log(a + b);
+}
+
+plus(10, 20);
+plus(20, 30);
